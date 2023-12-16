79d65ee1589a7e3b6ffc616fe44629dcb009b87a
Malek <zaag.malek1@gmail.com>
2023-12-16 10:39:19 +0100

    add Code

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type book struct {
@@ -11,15 +14,20 @@ type book struct {
 	Quantity int16  `json:"quantity"`
 }
 
-books := []books {
+var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
 
-
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
 
 func main() {
 	fmt.Println("hellow world !!!")
 	fmt.Println("your application is working")
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
 }
