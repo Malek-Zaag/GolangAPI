71a4aa9314b69f393404a6c9959ae4e4fa8287f7
Malek <zaag.malek1@gmail.com>
2023-12-16 10:24:24 +0100

    init

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int16  `json:"quantity"`
+}
+
+books := []books {
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+
 
 func main() {
 	fmt.Println("hellow world !!!")
