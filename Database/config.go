53f0bea8213d3b009d1c13f61f50ac6b98a76527
Malek <zaag.malek1@gmail.com>
2023-12-20 22:46:46 +0100

    database linked

---
+++ Database/config.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var Database *gorm.DB
+
+func Init() {
+	var err error
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, user, password, dbname, port)
+	Database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Successfully connected to the database")
+	}
+
+}
--- main.go
+++ main.go
@@ -1,103 +1,121 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"net/http"
+	"log"
+	database "main/Database"
+	model "main/Model"
 
-	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-var books = []book{
-	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
-	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
-	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
-}
-
-func getBooks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, books)
-}
-
-func bookById(c *gin.Context) {
-	id := c.Param("id")
-	book, err := getBookById(id)
-
+func loadEnv() {
+	err := godotenv.Load(".env.local")
 	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
-		return
+		log.Fatal("Error loading .env file")
 	}
-	c.IndentedJSON(http.StatusOK, book)
 }
 
-func getBookById(id string) (*book, error) {
-	for i, book := range books {
-		if book.ID == id {
-			return &books[i], nil
-		}
-	}
-	return nil, errors.New("book not found")
-}
+func loadDatabase() {
 
-func createBook(c *gin.Context) {
-	var newBook book
-	if err := c.BindJSON(&newBook); err != nil {
-		return
-	}
-	books = append(books, newBook)
-	c.IndentedJSON(http.StatusCreated, newBook)
-}
-
-func checkoutBook(c *gin.Context) {
-	id, ok := c.GetQuery("id")
-
-	if !ok {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
-		return
-	}
-
-	book, err := getBookById(id)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
-		return
-	}
-
-	if book.Quantity <= 0 {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available."})
-		return
-	}
-
-	book.Quantity -= 1
-	c.IndentedJSON(http.StatusOK, book)
-}
-
-func returnBook(c *gin.Context) {
-	id, ok := c.GetQuery("id")
-
-	if !ok {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
-		return
-	}
-
-	book, err := getBookById(id)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
-		return
-	}
-
-	book.Quantity += 1
-	c.IndentedJSON(http.StatusOK, book)
+	database.Init()
+	database.Database.AutoMigrate(&model.Book{})
 }
 
 func main() {
-	fmt.Println("hellow world !!!")
-	fmt.Println("your application is working")
-	router := gin.Default()
-	router.GET("/books", getBooks)
-	router.GET("books/:id", bookById)
-	router.POST("/books", createBook)
-	router.PATCH("/checkout", checkoutBook)
-	router.PATCH("/return", returnBook)
-	router.Run("localhost:8080")
+	loadEnv()
+	loadDatabase()
 }
+
+// var books = []Book{
+// 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+// 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+// 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+// }
+
+// func getBooks(c *gin.Context) {
+// 	c.IndentedJSON(http.StatusOK, books)
+// }
+
+// func bookById(c *gin.Context) {
+// 	id := c.Param("id")
+// 	book, err := getBookById(id)
+
+// 	if err != nil {
+// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+// 		return
+// 	}
+// 	c.IndentedJSON(http.StatusOK, book)
+// }
+
+// func getBookById(id string) (*book, error) {
+// 	for i, book := range books {
+// 		if book.ID == id {
+// 			return &books[i], nil
+// 		}
+// 	}
+// 	return nil, errors.New("book not found")
+// }
+
+// func createBook(c *gin.Context) {
+// 	var newBook book
+// 	if err := c.BindJSON(&newBook); err != nil {
+// 		return
+// 	}
+// 	books = append(books, newBook)
+// 	c.IndentedJSON(http.StatusCreated, newBook)
+// }
+
+// func checkoutBook(c *gin.Context) {
+// 	id, ok := c.GetQuery("id")
+
+// 	if !ok {
+// 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+// 		return
+// 	}
+
+// 	book, err := getBookById(id)
+
+// 	if err != nil {
+// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+// 		return
+// 	}
+
+// 	if book.Quantity <= 0 {
+// 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available."})
+// 		return
+// 	}
+
+// 	book.Quantity -= 1
+// 	c.IndentedJSON(http.StatusOK, book)
+// }
+
+// func returnBook(c *gin.Context) {
+// 	id, ok := c.GetQuery("id")
+
+// 	if !ok {
+// 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+// 		return
+// 	}
+
+// 	book, err := getBookById(id)
+
+// 	if err != nil {
+// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+// 		return
+// 	}
+
+// 	book.Quantity += 1
+// 	c.IndentedJSON(http.StatusOK, book)
+// }
+
+// func main() {
+// 	fmt.Println("hellow world !!!")
+// 	fmt.Println("your application is working")
+// 	router := gin.Default()
+// 	router.GET("/books", getBooks)
+// 	router.GET("books/:id", bookById)
+// 	router.POST("/books", createBook)
+// 	router.PATCH("/checkout", checkoutBook)
+// 	router.PATCH("/return", returnBook)
+// 	router.Run("localhost:8080")
+// }
